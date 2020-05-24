85cd0d462055e7869ad9147fb585e80df8a8bc41
fhashimoto <felipe.hashi@gmail.com>
2020-05-24 01:20:31 -0300

    Init project with typescript

---
+++ src/components/fixed/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="header-wrapper">
+    <v-app-bar dense dark :height="isMobile ? 50: 75">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          src="../../assets/elo-solar-logo.png"
+          :max-height="isMobile ? 40 : 65"
+          contain
+          position="center center"
+        ></v-img>
+      </template>
+    </v-app-bar>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data: () => ({
+    isMobile: false
+  }),
+
+  beforeDestroy() {
+    if (typeof window !== undefined) {
+      window.removeEventListener("resize", this.onResize);
+    }
+  },
+
+  mounted() {
+    this.onResize();
+    window.addEventListener("resize", this.onResize);
+  },
+
+  methods: {
+    onResize() {
+      this.isMobile = window.innerWidth < 960;
+    }
+  }
+});
+</script>
+
+<style>
+</style>
---
+++ src/components/login/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container grid-list-md>
+    <v-card raised>
+      <v-container grid-list-md>
+        <p class="title">Login</p>
+        <v-form v-model="valid">
+          <v-text-field
+            name="user"
+            label="Usuário"
+            autofocus
+            required
+            v-model="name"
+            :rules="[v => !!v || 'Informe um login']"
+          ></v-text-field>
+          <v-text-field
+            name="password"
+            label="Senha"
+            required
+            v-model="password"
+            :rules="[v => !!v || 'Informe uma senha']"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="showPassword ? 'text' : 'password'"
+            @click:append="showPassword = !showPassword"
+          ></v-text-field>
+          <v-btn rounded color="primary" :disabled="!valid" @click="tryLogin">Entrar</v-btn>
+        </v-form>
+      </v-container>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      valid: false,
+      name: "",
+      password: "",
+      showPassword: false
+    };
+  },
+
+  methods: {
+    tryLogin() {
+      console.log("TESTE");
+    }
+  }
+});
+</script>
+
+<style>
+</style>
